c86f8f5e22d59429f54644f15e0f2d2e1e1c5da5
Thomas <Tcarpane@asu.edu>
2017-11-11 11:34:01 -0700

    Add project files.

---
+++ InterDimensionalCable/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterDimensionalCable.Startup))]
+namespace InterDimensionalCable
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
