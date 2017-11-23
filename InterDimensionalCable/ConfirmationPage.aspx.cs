cfc012d9efa393fc08cf92dda44a83324e0de4ee
jfederic <jfederic@asu.edu>
2017-11-22 23:39:11 -0700

    learning to use git

--- InterDimensionalCable/ConfirmationPage.aspx.cs
+++ InterDimensionalCable/ConfirmationPage.aspx.cs
@@ -9,9 +9,21 @@ namespace InterDimensionalCable
 {
     public partial class ConfirmationPage : System.Web.UI.Page
     {
+        double salesTax = .0675;
+        double sh = .01;
+        double subtotal = 0.0;
+        double orderTotal = 0.0; 
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private double calculateOrderTotal(double subtotal)
+        {
+
+            orderTotal = subtotal + (subtotal * salesTax) + (subtotal * sh);
+            return orderTotal;
         }
     }
 }
